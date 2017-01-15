99d370145d1a7501e0d97a1db2b0edaa23bd15cd
scalka <calka.sylwia@gmail.com>
2017-01-15 14:06:27 +0000

    list view toggle

--- working version 1.0/js/javascript.js
+++ working version 1.0/js/javascript.js
@@ -262,6 +262,20 @@ function initMap() {
 			listview.innerHTML += ' <ul> <li> ' + locarray[i][4] +  ' </li> </ul> ';
 		}
 	}	
+
+
+	var piemenu = new wheelnav('piemenu');
+	piemenu.spreaderInTitle = icon.plus;
+	piemenu.spreaderOutTitle = icon.cross;
+	piemenu.spreaderRadius = piemenu.wheelRadius * 0.13;
+	piemenu.sliceInitPathFunction = piemenu.slicePathFunction;
+	piemenu.initPercent = 0.1;
+	piemenu.wheelRadius = piemenu.wheelRadius * 0.83;
+	piemenu.navItemsContinuous = true;
+	piemenu.sliceAngle = 45;
+	piemenu.createWheel();
+	piemenu.setTooltips(['0','1','2','3']);
+	
 } // init() close
 
 /*directionsDisplay.setDirections(response);
